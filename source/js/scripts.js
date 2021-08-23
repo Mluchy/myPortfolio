8ef81e84690113b8800e4555836ee26ff2128852
Mluchy <luchy.isi@gmail.com>
2021-08-23 17:00:32 -0300

    v0.1 - Portfolio - correcciones 5

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -4,38 +4,25 @@ const toggleSwitch = document.querySelector(
 );
 
 const sun = document.querySelector("#sun");
-sun.setAttribute("src", "source/img/icons/sun.png");
+sun.setAttribute("src", "./source/img/icons/sun.png");
 
 const moon = document.querySelector("#moon");
-moon.setAttribute("src", "source/img/icons/moon.png");
+moon.setAttribute("src", "./source/img/icons/moon.png");
 
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
-    sun.setAttribute("src", "source/img/icons/sun2.png");
-    moon.setAttribute("src", "source/img/icons/moon2.png");
+    sun.setAttribute("src", "./source/img/icons/sun2.png");
+    moon.setAttribute("src", "./source/img/icons/moon2.png");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
-    sun.setAttribute("src", "source/img/icons/sun.png");
-    moon.setAttribute("src", "source/img/icons/moon.png");
+    sun.setAttribute("src", "./source/img/icons/sun.png");
+    moon.setAttribute("src", "./source/img/icons/moon.png");
   }
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
 
-/* === Active  === */
-
-/* var header = document.getElementById("nav");
-var btns = header.getElementsByClassName("nav-link");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-    this.className += " active";
-  });
-} */
 
 /* === Nav responsive === */
 
